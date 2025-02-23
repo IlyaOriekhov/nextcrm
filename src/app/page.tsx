5d2621c4f56ff816b51da617fba8f1ca770de9da
Ilya Oriekhov <164690908+IlyaOriekhov@users.noreply.github.com>
2025-02-23 15:21:49 +0200

    routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import AddCompanyButton from '@/app/components/add-company-button';
-import ServerComponent from './components/server-component';
-import ClientComponent from './components/client-component';
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home page{new Date().toTimeString()}</h1>
+      <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
-      <ServerComponent />
-      <ClientComponent />
     </main>
   );
 }
